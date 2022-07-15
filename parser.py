cfee8196b530e3a56e9f1a4ba43d0d4d2ba248f8
ronteo <nimrack@gmail.com>
2022-07-15 16:34:58 +0800

    Update parser.py

--- parser.py
+++ parser.py
@@ -12,46 +12,32 @@ INPUT_FOLDER = "input"
 def fix_precision(s):
     return "%.2f" % float(s)
 
-# def main_old(*argv):
-#     header_written = False
-#     output_file = "output/%s.txt" % date.today()
-#     with open(output_file, "w+") as f:
-#         for arg in argv:
-#             print("Processing file:%s" % arg)
-#             for i, line in enumerate(open(arg)):
-#                 if i == 0:
-#                     assert line == EXPECTED_HEADER, "Error! Columns should be:\n%s\bFound:\n%s" % (EXPECTED_HEADER, line)
-#                     if not header_written:
-#                         f.write(line)
-#                         header_written = True
-#                 else:
-#                     arr = line.split(",")
-#                     for i in range(2, 6):
-#                         arr[i] = fix_precision(arr[i])
-#                     f.write(",".join(arr))
-#     print("Output written to: %s" % output_file)
-
 def main():
-    df = pd.DataFrame()
 
-    for f in os.listdir(INPUT_FOLDER):
-        df = df.append(pd.read_csv(os.path.join(INPUT_FOLDER,f)))
+    for dir in os.listdir(INPUT_FOLDER):
+        df = pd.DataFrame()
+        for f in os.listdir(os.path.join(INPUT_FOLDER, dir)):
+            df = df.append(pd.read_csv(os.path.join(INPUT_FOLDER, dir, f)))
 
-    df.columns = [c.replace("\"","").strip() for c in df.columns]
+        df.columns = [c.replace("\"", "").strip() for c in df.columns]
 
-    print(df.columns)
-    for c in PRICE_COLUMNS:
-        df[c] = df.apply(lambda x: fix_precision(x[c]), axis=1)
-    df['DateTypeCol'] = pd.to_datetime(df.Date)
-    df.sort_values(by=["DateTypeCol", "Time"], inplace=True)
-    del df['DateTypeCol']
-    output_file = "output/%s.txt" % date.today()
+        print(df.columns)
+        for c in PRICE_COLUMNS:
+            df[c] = df.apply(lambda x: fix_precision(x[c]), axis=1)
+        df['DateTypeCol'] = pd.to_datetime(df.Date)
+        df["Time"] = df.Time.apply(lambda x: int(str(x)[:4]))
+        df.sort_values(by=["DateTypeCol", "Time"], inplace=True)
+        df.drop_duplicates(subset=["Date", "Time"], inplace=True)
+        del df['DateTypeCol']
+        output_file = os.path.join("output", dir, str(date.today()) + ".txt")
+        
+        out_folder = os.path.split(output_file)[0]
+        if(not os.path.exists(out_folder)):
+            os.mkdir(out_folder)
+        
 
-    df.to_csv(output_file, index=False)
-    print("Output written to: %s" % output_file)
+        df.to_csv(output_file, index=False)
+        print("Output written to: %s" % output_file)
+
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser(description='Process market data.')
-    # parser.add_argument('files', metavar='f', type=str, nargs='+',
-    #                     help='path to files. files will be cleaned and appended in order to a single file')
-    # args = parser.parse_args()
     main()
