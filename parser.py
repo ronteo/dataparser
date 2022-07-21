e6a4e9ef742b3c4213dea525033c789b88c1fa9e
ronteo <nimrack@gmail.com>
2022-07-21 19:02:55 +0800

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,5 +1,5 @@
 import argparse
-from datetime import date
+from datetime import datetime, date
 import os
 
 import pandas as pd
@@ -17,24 +17,28 @@ def main():
     for dir in os.listdir(INPUT_FOLDER):
         df = pd.DataFrame()
         for f in os.listdir(os.path.join(INPUT_FOLDER, dir)):
-            df = df.append(pd.read_csv(os.path.join(INPUT_FOLDER, dir, f)))
-
-        df.columns = [c.replace("\"", "").strip() for c in df.columns]
+            filename = os.path.join(INPUT_FOLDER, dir, f)
+            header = None
+            for l in open(filename):
+                header = l
+                header = [h.replace("\"", "").strip() for h in header.split(",")]
+                break
+            df = df.append(pd.read_csv(filename, dtype={"Time": str}, names=header, skiprows=1))
 
         print(df.columns)
         for c in PRICE_COLUMNS:
             df[c] = df.apply(lambda x: fix_precision(x[c]), axis=1)
-        df['DateTypeCol'] = pd.to_datetime(df.Date)
-        df["Time"] = df.Time.apply(lambda x: int(str(x)[:4]))
-        df.sort_values(by=["DateTypeCol", "Time"], inplace=True)
+        df["Time"] = df.Time.apply(lambda x: str(x)[:4])
+        df["DateTimeTypeCol"] = df.apply(lambda x: datetime.strptime(str(x.Date) + x.Time, '%Y%m%d%H%M'), axis=1)
+        df.sort_values(by=["DateTimeTypeCol"], inplace=True)
         df.drop_duplicates(subset=["Date", "Time"], inplace=True)
-        del df['DateTypeCol']
+        del df['DateTimeTypeCol']
         output_file = os.path.join("output", dir, str(date.today()) + ".txt")
-        
+
         out_folder = os.path.split(output_file)[0]
         if(not os.path.exists(out_folder)):
             os.mkdir(out_folder)
-        
+
 
         df.to_csv(output_file, index=False)
         print("Output written to: %s" % output_file)
